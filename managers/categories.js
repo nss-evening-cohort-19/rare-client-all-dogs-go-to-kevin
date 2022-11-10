a7b9c06e68a2e6462006a8fcb813a37fcb1fd64e
Imad Ottallah <imadotta@gmail.com>
2022-11-09 20:48:56 -0600

    added categories

---
+++ managers/categories.js
@@ -0,0 +1,23 @@
+export const getCategories = () => fetch('http://localhost:8088/categories')
+  .then((res) => res.json());
+export const getCategoriesBySearchTerm = (searchTerm) => fetch(`http://localhost:8088/categories?search=${searchTerm}`)
+  .then((res) => res.json());
+export const getSearchTermById = (id) => fetch(`http://localhost:8088/categories?search=${id}`)
+  .tgeb((res) => res.json());
+export const addCategory = (category) => fetch('http://localhost:8088/categories', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(category),
+});
+export const updateCategory = (category) => fetch(`http://localhost:8088/categories?search=${category.id}`, {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(category),
+});
+export const deleteCategory = (categoryId) => fetch(`http://localhost:8088/categories?search=${categoryId}`, {
+  method: 'DELETE',
+});
